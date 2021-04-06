04dfddccf35c10769d791500949f6edd1ddf68d0
rafapgomes <pimentelgomes17@gmail.com>
2021-04-06 14:02:45 -0300

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import youtube
 import twitter
+import time
+import threading
 
 
 twitter_id ='1371881979252252681'
-youtube_id = "UCwMiGkjaLWcQn-XXvyQtW0A"
+boquinha_id = "UCwMiGkjaLWcQn-XXvyQtW0A"
+renato_id = 'UChMJkHCD2wdvSO7CMOuLR-w'
+leoruiz_id = 'UCCgZZd8zy-OyiVtQ5WeOV6w'
+thyreis_id = 'UCQ0IutRW590TSJUDyBPP2Pw'
+gui_id = 'UC_XGgRFoktKUXZVD1_tp7EA'
 
 def getytvideoid(yt_id):
     response = youtube.GetchannelInfo(yt_id)
@@ -12,23 +18,37 @@ def getytvideoid(yt_id):
     video = youtube.Getvideoid(videos_res)
     return video
 
+def compara(num,vet):
+    print(vet)
+    for vid_id in vet:
+        if vid_id == num:
+            return 1
+        
 
-def posttweet(tt_id):
+def tweet(name,tt_id,ytbr_id):
+    num = getytvideoid(ytbr_id)
     timeline = twitter.searchownTL(tt_id)
     vetor = []
     vetor = twitter.getvideourl(timeline)
     split = []
     split = twitter.splitvetor(vetor)
-    for  vid_id in split:
-        if getytvideoid(youtube_id) != vid_id:
-            texto = 'https://www.youtube.com/watch?v='+ getytvideoid(youtube_id)
-            twitter.posttweet(texto)
-            return
-        else: 
-            print('Video nao enviado')
-            return
-
+    teste = compara(num,split)
+    if teste != 1:
+        twitter.posttweet(name +" postou um novo video "+" https://www.youtube.com/watch?v="+num)
+    else:
+         print("Video ja enviado")       
             
 
-posttweet(twitter_id)
 
+def main(name,ytbr_id):
+        print(name)
+        tweet(name,twitter_id,ytbr_id)
+
+
+while True: 
+    main('Renato',renato_id)
+    main('Boquinha',boquinha_id)
+    main('Leo',leoruiz_id)
+    main('Thi',thyreis_id)
+    main('Gui',gui_id)
+    time.sleep(10)
