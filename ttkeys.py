8b05367a8f9671a8e07e0b7e3cdd3d439ba7e40a
rafapgomes <pimentelgomes17@gmail.com>
2021-04-08 09:42:43 -0300

    keys

--- ttkeys.py
+++ ttkeys.py
@@ -1,7 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv("ttkeys.env")
 
 consumer_key = os.environ.get("consumer_key")
 consumer_secret = os.environ.get("consumer_secret")
--- ytkeys.py
+++ ytkeys.py
@@ -1,7 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv("ytkeys.env")
 
 API_KEY = os.environ.get("API_KEY")
 
